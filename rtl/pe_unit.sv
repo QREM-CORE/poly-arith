2e51538de1ceecad7a3e78ea0aaa908cc7e692ee
Salwan Aldhahab <91484919+salwan-aldhahab@users.noreply.github.com>
2026-03-10 16:39:23 -0400

    Revert "Update pe_unit.sv"
    This reverts commit 878aaea17442f3959b873127129c1a40d469c270.

--- rtl/pe_unit.sv
+++ rtl/pe_unit.sv
@@ -331,8 +331,7 @@ module pe_unit (
 
                 // Simulation-only error catching for invalid control states
                 // synthesis translate_off
-                if ($time > 0)
-                    $error("[AU Wrapper] ERROR: Invalid pe_mode_e state received: %b", ctrl_i);
+                $error("[AU Wrapper] ERROR: Invalid pe_mode_e state received: %b", ctrl_i);
                 // synthesis translate_on
             end
         endcase
